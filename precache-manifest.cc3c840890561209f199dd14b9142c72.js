eb0744c802417e965e8b17f3374141d4c82297bc
Daniel Cassar <dcassar622@gmail.com>
2020-03-18 18:40:17 +0000

    Updates

---
+++ precache-manifest.cc3c840890561209f199dd14b9142c72.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5711449a2cb6aa308b6c763ffb4c243a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "675cb6459414e8d32041",
+    "url": "/static/css/main.3e7468d0.chunk.css"
+  },
+  {
+    "revision": "d7f6b6b17d29b97526b5",
+    "url": "/static/js/2.5f209e8e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.5f209e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675cb6459414e8d32041",
+    "url": "/static/js/main.64c18fba.chunk.js"
+  },
+  {
+    "revision": "6ef5fe402e534b5d4b41",
+    "url": "/static/js/runtime-main.c43d354e.js"
+  },
+  {
+    "revision": "0bb6502ef021a773a618f922ad03d2d2",
+    "url": "/static/media/background-main-small.0bb6502e.gif"
+  },
+  {
+    "revision": "de079e6ae7fbddb91420f3be65782aca",
+    "url": "/static/media/count-of-monte-crypto.de079e6a.png"
+  },
+  {
+    "revision": "6a7d7103fb35b90905541d125a28c7ab",
+    "url": "/static/media/me.6a7d7103.jpg"
+  },
+  {
+    "revision": "26670fb084ed0ed07af7506c20a4ee2b",
+    "url": "/static/media/portfolio-website.26670fb0.png"
+  }
+]);
